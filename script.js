d1ea7beb3ee57ba65dc61758f6b23d6ef900adde
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2024-07-14 15:12:09 +0900

    fix #3 game over

--- script.js
+++ script.js
@@ -56,21 +56,32 @@ function draw() {
   drawBall();
   drawPaddle();
 
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+  if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+
+  if (y + dy < ballRadius) {
     dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      alert("GAME OVER");
+      document.location.reload();
+      clearInterval(interval);
+    }
   }
 
-  if (rightPressed) {
-    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
-  } else if (leftPressed) {
-    paddleX = Math.max(paddleX - 7, 0);
+  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
   }
+  else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
 
   x += dx;
   y += dy;
 }
 
-setInterval(draw, 10);
+const interval = setInterval(draw, 10);
