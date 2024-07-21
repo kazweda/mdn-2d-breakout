f309ae62b26170e534f3d366ed705630e51989da
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2024-07-21 13:05:14 +0900

    fix #7

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ for (let c = 0; c < brickColumnCount; c++) {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
   if (e.key === "Right" || e.key === "ArrowRight") {
@@ -45,6 +46,14 @@ function keyUpHandler(e) {
     leftPressed = false;
   }
 }
+
+function mouseMoveHandler(e) {
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+}
+
 
 function collisionDetection() {
   for (let c = 0; c < brickColumnCount; c++) {
